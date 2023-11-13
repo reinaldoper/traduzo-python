0ae5ac71d1908d484018c94365e54bbf0b7a6384
reinaldoper <reinaldoper83@gmail.com>
2023-11-13 06:26:06 -0400

    [Reinaldo] req 4

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request
 from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 from models.history_model import HistoryModel
-from database.seed_language import seed_language
+import run_seeds 
 
 
 translate_controller = Blueprint("translate_controller", __name__)
@@ -13,56 +13,79 @@ translate_controller = Blueprint("translate_controller", __name__)
 def index():
     languages = query()
     if request.method == "POST":
-        text_to_translate = request.form.get("text-to-translate")
-        translate_from = request.form.get("translate-from")
-        translate_to = request.form.get("translate-to")
-
         HistoryModel({
-            "text_to_translate": text_to_translate,
-            "translate_from": translate_from,
-            "translate_to": translate_to
+            "text_to_translate": request.form.get("text-to-translate"),
+            "translate_from": request.form.get("translate-from"),
+            "translate_to": request.form.get("translate-to")
         }).save()
 
         data = {
-            'languages': languages,
             'text_to_translate': request.form.get("text-to-translate"),
             'translate_from': request.form.get("translate-from"),
             'translate_to': request.form.get("translate-to"),
             'translated': GoogleTranslator(
-                                source=translate_from,
-                                target=translate_to).
-            translate(text_to_translate)
+                                source=request.form.get("translate-from"),
+                                target=request.form.get("translate-to")).
+            translate(request.form.get("text-to-translate"))
             }
 
-        return render_template("index.html", data=data)
+        return render_template("index.html", data=data, languages=languages)
 
     if request.method == 'GET':
         data = {
-            'languages': languages,
             'text_to_translate': "O que deseja traduzir?",
             'translate_from': 'pt',
             'translate_to': 'en',
             'translated': "What do you want to translate?"
         }
-        return render_template('index.html', data=data)
+        return render_template('index.html', data=data, languages=languages)
 
 
 # Req. 6
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse():
-    raise NotImplementedError
+    languages = query()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    inverted_translate_from = translate_to
+    inverted_translate_to = translate_from
+
+    HistoryModel({
+        "text_to_translate": text_to_translate,
+        "translate_from": inverted_translate_to,
+        "translate_to": inverted_translate_from
+    }).save()
+
+    translated_text = GoogleTranslator(
+        source=inverted_translate_from,
+        target=inverted_translate_to
+    ).translate(text_to_translate)
+
+    data = {
+        'text_to_translate': text_to_translate,
+        'translate_from': inverted_translate_from,
+        'translate_to': inverted_translate_to,
+        'translated': translated_text
+    }
+    """ print(f"text_to_translate: {text_to_translate}")
+    print(f"translate_from: {translate_from}")
+    print(f"translate_to: {translate_to}")
+    print(f"inverted_translate_from: {inverted_translate_from}")
+    print(f"inverted_translate_to: {inverted_translate_to}")
+    print(f"translated_text: {translated_text}") """
+
+    return render_template("index.html", data=data, languages=languages)
 
 
 def query():
-    seed_language()
+    run_seeds.seed_language()
     languages = LanguageModel.list_dicts()
     obj = []
     for lingua in languages:
-        if lingua['name'] == 'english':
-            obj.append(lingua)
-        if lingua['name'] == 'afrikaans':
-            obj.append(lingua)
         if lingua['name'] == 'portuguese':
-            lingua['name'] = 'portugues'
+            lingua['name'] = 'PORTUGUES'
+            obj.append(lingua)
+        else:
             obj.append(lingua)
     return obj
