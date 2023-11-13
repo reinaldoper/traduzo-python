dc1ca4895a97fffcc13de08eb68cbc91de82a0ed
reinaldoper <reinaldoper83@gmail.com>
2023-11-12 20:12:35 -0400

    [Reinaldo] iniciando projeto req 4

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 # from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
+from models.language_model import LanguageModel
 # from models.history_model import HistoryModel
 
 
@@ -10,7 +10,15 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    languages = LanguageModel.find()
+    data = {
+        'languages': languages,
+        'text_to_translate': "O que deseja traduzir?",
+        'translate_from': 'pt',
+        'translate_to': 'en',
+        'translated': "What do you want to translate?"
+    }
+    return render_template('index.html', data=data)
 
 
 # Req. 6
