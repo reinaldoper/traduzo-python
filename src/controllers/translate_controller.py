587e49b776c8bd66da5519363e04d2bebf7dabfa
reinaldoper <reinaldoper83@gmail.com>
2023-11-12 21:57:55 -0400

    [Reinaldo] popula o banco

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request
 from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 from models.history_model import HistoryModel
+from database.seed_language import seed_language
 
 
 translate_controller = Blueprint("translate_controller", __name__)
@@ -10,7 +11,9 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
+    seed_language()
     languages = LanguageModel.list_dicts()
+    print(languages)
     if request.method == "POST":
         text_to_translate = request.form.get("text-to-translate")
         translate_from = request.form.get("translate-from")
